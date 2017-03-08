8ce38dd3446d483730aeb41c03302dc347c8a480
AndreiTodor <bvandrei94@gmail.com>
2017-03-08 11:20:18 +0100

    show files in dirView

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,26 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QDesktopServices>
+#include <iostream>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    dirModel = new QFileSystemModel(this);
+    fileModel = new QFileSystemModel(this);
+    //std::cout<<QDir::homePath().toStdString()<<std::endl;
+    QString rootPath = QDir::homePath();
+    dirModel->setRootPath(rootPath);
+    dirModel->setFilter(QDir::NoDotAndDotDot | QDir::AllDirs);
+    QModelIndex index = dirModel->index(rootPath);
+    ui->dirView->setModel(dirModel);
+    ui->dirView->setRootIndex(index);
+    ui->dirView->hideColumn(1);
+    ui->dirView->hideColumn(2);
+    ui->dirView->hideColumn(3);
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QFileSystemModel>
 
 namespace Ui {
 class MainWindow;
@@ -17,6 +18,8 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    QFileSystemModel* dirModel;
+    QFileSystemModel* fileModel;
 };
 
 #endif // MAINWINDOW_H
